1f74872d0eabed0cde3bb82b01bc70cce24039bf
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-23 23:57:39 +0530

    Time: 3 ms (56.08%), Space: 56.3 MB (80.6%) - LeetHub

--- 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.java
@@ -10,16 +10,10 @@ class Solution {
             if(nums[max]<nums[i])
                 max = i;
         }
-        System.out.println(min + " " + max);
         int b = Math.min(min,max)+1 + Math.abs(max-min);
-        System.out.print(b);
-        int e = Math.min(len-min,len-max) + Math.abs(min-max);
-        System.out.print(b);
+        int e = Math.min(len-min,len-max) + Math.abs(min-max);;
         int m1 = (min+1) + (len-max);
-        System.out.print(b);
         int m2 = (len-min) + (max+1);
-        System.out.print(b);
-        System.out.println();
         return Math.min(b,Math.min(e,Math.min(m1,m2)));
     }
 }
