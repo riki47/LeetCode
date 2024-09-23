c9dde3b4684a9dca8948c56ec7dd50e6956d86a1
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-23 23:41:28 +0530

    Time: 27 ms (5.1%), Space: 56.3 MB (92.97%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,41 +1,34 @@
-// class Solution {
-//     public int firstMissingPositive(int[] nums) {
-//         HashMap <Integer,Integer> h1 = new HashMap<>();
-//         int c = 1;
-//         for(int i : nums)
-//         {
-//             if(i>0)
-//             {
-                
-//                 if(h1.containsKey(i))
-//                 {
-//                     continue;
-//                 }
-//                 if(c != i)
-//                 {
-//                     return c;
-//                 }
-//                 h1.put(i,0);
-//                 c++;
-//             }
-            
-//         }
-
-//         return c;
-//     }
-// }
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> h1 = new HashSet<>();
-        for(int i : nums)
-        {   
-            h1.add(i);
+        int[] filteredNums = Arrays.stream(nums).filter(n -> n > 0).toArray();
+        
+        Arrays.sort(filteredNums);
+        
+        int target = 1;
+        for (int n : filteredNums) {
+            if (n == target) {
+                target++;
+            } else if (n > target) {
+                return target;
+            }
         }
-        for(int i = 1;i<Integer.MAX_VALUE;i++)
-        {
-            if(!h1.contains(i))
-                return i;
-        }
-        return -1;
+        
+        return target;        
     }
 }
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         int min = 1;
+//         HashSet<Integer> h1 = new HashSet<>();
+//         for(int i : nums)
+//         {   
+//             h1.add(i);
+//         }
+//         while(true)
+//         {
+//             if(!h1.contains(min))
+//                 return min;
+//             min++;
+//         }
+//     }
+// }
