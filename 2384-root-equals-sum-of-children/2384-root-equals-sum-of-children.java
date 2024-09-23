b650b4e09a9efda196b9da9bf0fd2d878bf88281
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-23 10:35:16 +0530

    Time: 0 ms (100%), Space: 41.5 MB (12.22%) - LeetHub

--- 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.java
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.java
@@ -1,17 +1,22 @@
+// class Solution {
+//     public static int dfs( TreeNode root)
+//     {
+//         if(root == null)
+//             return 0; 
+//         if(root.left == null && root.right == null)
+//             return root.val;
+//         int a = dfs(root.left);
+//         int b = dfs(root.right);
+//         return a+b;
+//     }
+//     public boolean checkTree(TreeNode root) {
+//         if(root.val == dfs(root))
+//             return true;
+//         return false;
+//     }
+// }
 class Solution {
-    public static int dfs( TreeNode root)
-    {
-        if(root == null)
-            return 0; 
-        if(root.left == null && root.right == null)
-            return root.val;
-        int a = dfs(root.left);
-        int b = dfs(root.right);
-        return a+b;
-    }
     public boolean checkTree(TreeNode root) {
-        if(root.val == dfs(root))
-            return true;
-        return false;
+       return root.val == root.right.val + root.left.val; 
     }
 }
