2f738d5018ac0320f0015f4cbe50457a0c4e6fbd
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-23 23:31:52 +0530

    Time: 13 ms (29.71%), Space: 60.1 MB (9.93%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,44 @@
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         HashMap <Integer,Integer> h1 = new HashMap<>();
+//         int c = 1;
+//         for(int i : nums)
+//         {
+//             if(i>0)
+//             {
+                
+//                 if(h1.containsKey(i))
+//                 {
+//                     continue;
+//                 }
+//                 if(c != i)
+//                 {
+//                     return c;
+//                 }
+//                 h1.put(i,0);
+//                 c++;
+//             }
+            
+//         }
+
+//         return c;
+//     }
+// }
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        HashSet<Integer> h1 = new HashSet<>();
+        for(int i : nums)
+        {   
+            h1.add(i);
+            if(min>i && i>0)min = i;
+        }
+        min = 1;
+        while(true)
+        {
+            if(!h1.contains(min))
+                return min;
+            min++;
+        }
+    }
+}
