156487429a57ac84aa637ad3024e89f64ff800e6
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-25 14:10:18 +0530

    Time: 1 ms (95.08%), Space: 44.7 MB (32.57%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,42 +1,35 @@
-// class Solution {
-//     public void duplicateZeros(int[] arr) {
-//         int newa[] = new int[arr.length];
-//         int idx = 0;
-//         for(int i : arr)
-//         {
-//             if(idx>=arr.length)
-//                 break;
-//             if(i == 0)
-//             {
-//                 newa[idx] = 0;
-//                 idx++;
-//             }
-//             if(idx>=arr.length)
-//                 break;
-//             newa[idx] = i;
-//             idx++;
-//         }
-//         idx = 0;
-//         for(int i : newa)
-//         {
-//             arr[idx] = i;
-//             idx++;
-//         }
-//     }
-// }
 class Solution {
-     public void duplicateZeros(int[] arr) {
-
-        int[] tempArr = new int[arr.length];
-         
-        for (int arrIndex = 0, tempIndex = 0; tempIndex < tempArr.length; arrIndex++) {
-            tempArr[tempIndex] = arr[arrIndex];
-
-            if (tempArr[tempIndex++] == 0 && tempIndex < tempArr.length) {
-                tempArr[tempIndex++] = 0;
+    public void duplicateZeros(int[] arr) {
+        int newa[] = new int[arr.length];
+        int idx = 0;
+        for(int i  = 0;idx<arr.length;i++)
+        {
+            if(arr[i] == 0)
+            {
+                newa[idx] = 0;
+                idx++;
             }
+            if(idx>=arr.length)
+                break;
+            newa[idx] = arr[i];
+            idx++;
         }
-         
-        System.arraycopy(tempArr, 0, arr, 0, arr.length);
+        System.arraycopy(newa, 0, arr, 0, arr.length);
     }
 }
+// class Solution {
+//      public void duplicateZeros(int[] arr) {
+
+//         int[] tempArr = new int[arr.length];
+         
+//         for (int arrIndex = 0, tempIndex = 0; tempIndex < tempArr.length; arrIndex++) {
+//             tempArr[tempIndex] = arr[arrIndex];
+
+//             if (tempArr[tempIndex++] == 0 && tempIndex < tempArr.length) {
+//                 tempArr[tempIndex++] = 0;
+//             }
+//         }
+         
+//         System.arraycopy(tempArr, 0, arr, 0, arr.length);
+//     }
+// }
