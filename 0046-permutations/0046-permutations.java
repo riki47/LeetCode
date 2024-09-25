7f11a0b4d3e84ff9c2297071d3e34a82f204328e
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-25 23:21:57 +0530

    Time: 1 ms (94.46%), Space: 44.7 MB (21.19%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,52 +1,53 @@
-// class Solution {
-//     static void comb(int nums[],ArrayList<Integer> a1,List<List<Integer>> l1)
-//     {
-//         if(nums.length <= 0)
-//         {
-//             l1.add(new ArrayList(a1));
-//             return;
-//         }
-//         for(int idx = 0; idx<nums.length; idx++)
-//         {   
-//             a1.add(nums[idx]);
-//             int[] firstPart = Arrays.copyOfRange(nums, 0, idx);     
-//             int[] secondPart = Arrays.copyOfRange(nums, idx + 1, nums.length); 
-//             int[] result = new int[firstPart.length + secondPart.length];
-//             System.arraycopy(firstPart, 0, result, 0, firstPart.length);
-//             System.arraycopy(secondPart, 0, result, firstPart.length, secondPart.length);
-//             comb(result,a1,l1);
-//             a1.removeLast();
-//         }
-//     }
-//     public List<List<Integer>> permute(int[] nums) {
-//         List<List<Integer>> l1 = new ArrayList<>();
-//         comb(nums, new ArrayList<>(),l1);
-//         return l1;
-//     }
-// }
-
 class Solution {
     List<List<Integer>> l1 ;
-    void comb(int nums[],ArrayList <Integer> a1,int i)
+    void comb(int nums[],ArrayList<Integer> a1)
     {
-        if(a1.size() >= nums.length)
+        if(nums.length <= 0)
         {
             l1.add(new ArrayList(a1));
             return;
         }
-        for(int idx = i; idx<nums.length; idx++)
+        for(int idx = 0; idx<nums.length; idx++)
         {   
-            if(!a1.contains(nums[idx]))
-            {
-                a1.add(nums[idx]);
-                comb(nums,a1,i);
-                a1.remove(a1.size()-1);
-            }
+            a1.add(nums[idx]);
+            int[] firstPart = Arrays.copyOfRange(nums, 0, idx);     
+            int[] secondPart = Arrays.copyOfRange(nums, idx + 1, nums.length); 
+            int[] result = new int[firstPart.length + secondPart.length];
+            System.arraycopy(firstPart, 0, result, 0, firstPart.length);
+            System.arraycopy(secondPart, 0, result, firstPart.length, secondPart.length);
+            comb(result,a1);
+            a1.removeLast();
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        l1 = new ArrayList<>();
-        comb(nums, new ArrayList<>(),0);
+        l1  = new ArrayList<>();
+        comb(nums, new ArrayList<>());
         return l1;
     }
 }
+
+// class Solution {
+//     List<List<Integer>> l1 ;
+//     void comb(int nums[],ArrayList <Integer> a1,int i)
+//     {
+//         if(a1.size() >= nums.length)
+//         {
+//             l1.add(new ArrayList(a1));
+//             return;
+//         }
+//         for(int idx = i; idx<nums.length; idx++)
+//         {   
+//             if(!a1.contains(nums[idx]))
+//             {
+//                 a1.add(nums[idx]);
+//                 comb(nums,a1,i);
+//                 a1.remove(a1.size()-1);
+//             }
+//         }
+//     }
+//     public List<List<Integer>> permute(int[] nums) {
+//         l1 = new ArrayList<>();
+//         comb(nums, new ArrayList<>(),0);
+//         return l1;
+//     }
+// }
