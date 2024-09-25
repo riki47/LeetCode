2778b8cea3b27f451c0df69e601ccb6a0205e2bb
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-25 13:22:08 +0530

    Time: 3 ms (98.07%), Space: 44 MB (88.42%) - LeetHub

--- 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.java
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.java
@@ -1,24 +1,46 @@
+// class Solution {
+//     public int deleteGreatestValue(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int temp = 0;
+//         int fmax = 0;
+//         for(int i = 0;i<n;i++)
+//         {
+//             for(int j = 0;j<m;j++)
+//             {
+//                 int max = 0;
+//                 for(int k = 1;k<n;k++)
+//                 {
+//                     if(grid[j][k]>grid[j][max])
+//                         max = k;
+//                 }
+//                 temp = Math.max(temp,grid[j][max]);
+//                 grid[j][max] = -1;
+//             }
+//             fmax+=temp;
+//             temp = 0;
+//         }
+//         return fmax;
+//     }
+// }
+
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
         int m = grid.length;
-        int n = grid[0].length;
-        int temp = 0;
+        int n = grid[0].length-1;
         int fmax = 0;
-        for(int i = 0;i<n;i++)
+        int temp = 0;
+        for(int i = 0;i<m;i++)
+            Arrays.sort(grid[i]);
+        for(int j = n;j>=0;j--)
         {
-            for(int j = 0;j<m;j++)
-            {
-                int max = 0;
-                for(int k = 1;k<n;k++)
-                {
-                    if(grid[j][k]>grid[j][max])
-                        max = k;
-                }
-                temp = Math.max(temp,grid[j][max]);
-                grid[j][max] = -1;
-            }
-            fmax+=temp;
             temp = 0;
+            for(int i = 0;i<m;i++)
+            {
+                if(temp<grid[i][j])
+                    temp = grid[i][j];
+            }
+            fmax += temp;
         }
         return fmax;
     }
