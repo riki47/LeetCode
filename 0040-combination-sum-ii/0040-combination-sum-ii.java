e72d63fb6e4f8241b9c7690671f4d64ce53672a1
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-25 23:50:16 +0530

    Time: 2 ms (99.27%), Space: 43.1 MB (58.47%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,65 @@
+// class Solution {
+//     static boolean recur(ArrayList<List<Integer>> l1,ArrayList<Integer> a1,int num[],int i,int sum,int target)
+//     {
+//         if(sum == target)
+//         {
+//             l1.add(new ArrayList<>(a1));
+//             return false;
+//         }
+//         if(i>=num.length || sum > target)
+//         {
+//             return false;
+//         }
+//         a1.add(num[i]);
+//         boolean a = recur(l1,a1,num,i+1,sum+num[i],target);
+//         a1.remove(a1.size()-1);
+//         if(a == false)
+//             return true;
+//         while(i < num.length-1 && num[i] == num[i+1])
+//             i++;
+//         recur(l1,a1,num,i+1,sum,target);
+//         return true;
+//     }
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) 
+//     {
+//         Arrays.sort(candidates);
+//         ArrayList<List<Integer>> h1 = new ArrayList<>();
+//         recur(h1,new ArrayList<>(),candidates,0,0,target);
+//         return h1;
+//     }
+// }
+
+class Solution {
+    static void recur(ArrayList<List<Integer>> l1, ArrayList<Integer> a1, int num[], int sum, int idx, int target) 
+    {
+        if(sum == target)
+        {
+            l1.add(new ArrayList(a1));
+            return;
+        }
+        if(sum>target)
+        {
+            return;
+        }
+        if(idx>=num.length)
+        {
+            return;
+        }
+        for(int i = idx;i<num.length;i++)
+        {
+            if(i>idx && num[i] == num[i-1])continue;
+            if(i>=num.length || sum+num[i]>target)break;
+
+            a1.add(num[i]);
+            recur(l1,a1,num,sum+num[i],i+1,target);
+            a1.removeLast();
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) 
+    {
+        Arrays.sort(candidates);
+        ArrayList<List<Integer>> h1 = new ArrayList<>();
+        recur(h1,new ArrayList<>(),candidates,0,0,target);
+        return h1;
+    }
+}
