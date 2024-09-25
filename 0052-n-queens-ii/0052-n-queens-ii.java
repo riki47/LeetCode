3779dc5fdff0e5aaac44140970f7bd8f46750d4e
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-25 22:24:16 +0530

    Time: 2 ms (68.03%), Space: 40.3 MB (58.02%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,3 +1,58 @@
+// class Solution {
+//     public static int co = 0;
+//     static boolean check(int mat[][],int row,int col)
+//     {
+//         int n = mat.length;
+//         for(int r = row;r>=0;r--)
+//         {
+//             if(mat[r][col] == 1)
+//                 return false;
+//         }
+//         for(int c = col;c>=0;c--)
+//         {
+//             if(mat[row][c] == 1)
+//                 return false;
+//         }
+//         for(int c = col, r = row; c >= 0 && r >= 0; c--,r--) 
+//         {
+//             if(mat[r][c] == 1)
+//                 return false;
+//         }
+//         for(int c = col, r = row; c >= 0 && r < n ; c--,r++) 
+//         {
+//             if(mat[r][c] == 1)
+//                 return false;
+//         }
+//         return true;
+//     }
+//     static void solqueen(int mat[][],int c)
+//     {
+//         if(c == mat.length)
+//         {
+//             co++;
+//             return;
+//         }
+//         if(c < mat.length)
+//         {
+//             for(int  r = 0; r<mat.length; r++)
+//             {
+//                 if(check(mat,r,c))
+//                 {
+//                     mat[r][c] = 1;
+//                     solqueen(mat,c+1);
+//                     mat[r][c] = 0;
+//                 }
+//             }
+//         }
+        
+//     }
+//     public int totalNQueens(int n) {
+//         co = 0;
+//         int mat[][] = new int[n][n];
+//         solqueen(mat,0);
+//         return co;
+//     }
+// }
 class Solution {
     public static int co = 0;
     static boolean check(int mat[][],int row,int col)
