b95a582f796796183c0fdc835cdc739f2d26ca77
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-25 14:05:23 +0530

    Time: 0 ms (100%), Space: 44.2 MB (97.83%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,26 +1,42 @@
+// class Solution {
+//     public void duplicateZeros(int[] arr) {
+//         int newa[] = new int[arr.length];
+//         int idx = 0;
+//         for(int i : arr)
+//         {
+//             if(idx>=arr.length)
+//                 break;
+//             if(i == 0)
+//             {
+//                 newa[idx] = 0;
+//                 idx++;
+//             }
+//             if(idx>=arr.length)
+//                 break;
+//             newa[idx] = i;
+//             idx++;
+//         }
+//         idx = 0;
+//         for(int i : newa)
+//         {
+//             arr[idx] = i;
+//             idx++;
+//         }
+//     }
+// }
 class Solution {
-    public void duplicateZeros(int[] arr) {
-        int newa[] = new int[arr.length];
-        int idx = 0;
-        for(int i : arr)
-        {
-            if(idx>=arr.length)
-                break;
-            if(i == 0)
-            {
-                newa[idx] = 0;
-                idx++;
+     public void duplicateZeros(int[] arr) {
+
+        int[] tempArr = new int[arr.length];
+         
+        for (int arrIndex = 0, tempIndex = 0; tempIndex < tempArr.length; arrIndex++) {
+            tempArr[tempIndex] = arr[arrIndex];
+
+            if (tempArr[tempIndex++] == 0 && tempIndex < tempArr.length) {
+                tempArr[tempIndex++] = 0;
             }
-            if(idx>=arr.length)
-                break;
-            newa[idx] = i;
-            idx++;
         }
-        idx = 0;
-        for(int i : newa)
-        {
-            arr[idx] = i;
-            idx++;
-        }
+         
+        System.arraycopy(tempArr, 0, arr, 0, arr.length);
     }
 }
