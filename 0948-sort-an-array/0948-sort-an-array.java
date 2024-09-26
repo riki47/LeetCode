4f44ea13602a9b26fb4e3339cf4fa8e714706cdf
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-26 18:11:15 +0530

    Time: 19 ms (79.12%), Space: 55.7 MB (65.09%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,15 +1,21 @@
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         Queue<Integer> q1 = new PriorityQueue<>();
+//         for(int i : nums)
+//         {
+//             q1.offer(i);
+//         }
+//         int newn[] = new int[nums.length];
+//         for(int i = 0 ; i<nums.length; i++)
+//         {
+//             newn[i] = q1.poll();
+//         }
+//         return newn;
+//     }
+// }
 class Solution {
     public int[] sortArray(int[] nums) {
-        Queue<Integer> q1 = new PriorityQueue<>();
-        for(int i : nums)
-        {
-            q1.offer(i);
-        }
-        int newn[] = new int[nums.length];
-        for(int i = 0 ; i<nums.length; i++)
-        {
-            newn[i] = q1.poll();
-        }
-        return newn;
+        Arrays.sort(nums);
+        return nums;
     }
 }
