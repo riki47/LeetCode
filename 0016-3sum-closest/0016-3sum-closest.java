15d69bfcffd82781dbf360cbe840851f5233d4ac
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-26 12:59:11 +0530

    Time: 11 ms (98.55%), Space: 42.8 MB (85.5%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -6,6 +6,7 @@ class Solution {
         Arrays.sort(nums);
         for(int i = 0; i<n; i++)
         {
+            if(i>0 && nums[i] == nums[i-1])continue;
             int j = i+1;
             int k = n-1;
             while(j<k)
