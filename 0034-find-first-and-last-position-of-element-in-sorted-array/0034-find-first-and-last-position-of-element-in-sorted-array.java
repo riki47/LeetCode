0ccb1eae5f2e9007129f84cf908581ea7f255365
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-27 13:05:43 +0530

    Time: 0 ms (100%), Space: 45.8 MB (51.36%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,28 +1,6 @@
 class Solution {
     int first = -1;
     int second = -1;
-    boolean found = false;
-    void binarys(int[] nums, int target, int start , int end)
-    {
-        if(start<=end)
-        {
-            int mid = ((end+start)/2);
-            if(nums[mid]>target)
-            {
-                binarys(nums,target,start ,mid-1);
-            }   
-            else if(nums[mid]<target)
-            {
-                first = mid;
-                binarys(nums,target,mid+1 ,end);
-            }
-            else
-            {
-                found = true;
-                return;
-            }
-        }
-    }
     void binary(int[] nums, int target, int start , int end)
     {
         if(start<=end)
@@ -56,11 +34,10 @@ class Solution {
         }
     }
     public int[] searchRange(int[] nums, int target) {
-        binarys(nums,target,0,nums.length-1);
-
-        if(!found)
-            return new int[]{-1,-1};
         binary(nums,target,0,nums.length-1);
+        if(first == -1 || nums[first] != target)
+            return new int[]{-1,-1};
+        
         binaryc(nums,target,0,nums.length-1);
         return new int[]{second,first};
     }
