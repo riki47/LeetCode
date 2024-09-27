6f7910ea92dccfde6e3369f0504ba1b9b3eb3b3b
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-27 12:34:10 +0530

    Time: 0 ms (100%), Space: 46.2 MB (9.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,67 @@
 class Solution {
-    static int binary(int[] nums, int target, int start , int end)
+    int first = -1;
+    int second = -1;
+    boolean found = false;
+    void binarys(int[] nums, int target, int start , int end)
     {
         if(start<=end)
         {
             int mid = ((end+start)/2);
             if(nums[mid]>target)
             {
-                return binary(nums,target,start ,mid-1);
+                binarys(nums,target,start ,mid-1);
             }   
             else if(nums[mid]<target)
             {
-                return binary(nums,target,mid+1 ,end);
+                first = mid;
+                binarys(nums,target,mid+1 ,end);
             }
             else
             {
-                return mid;
+                found = true;
+                return;
             }
         }
-        return -1;
+    }
+    void binary(int[] nums, int target, int start , int end)
+    {
+        if(start<=end)
+        {
+            int mid = ((end+start)/2);
+            if(nums[mid]>target)
+            {
+                binary(nums,target,start ,mid-1);
+            }   
+            else if(nums[mid]<=target)
+            {
+                first = mid;
+                binary(nums,target,mid+1 ,end);
+            }
+        }
+    }
+    void binaryc(int[] nums, int target, int start , int end)
+    {
+        if(start<=end)
+        {
+            int mid = ((end+start)/2);
+            if(nums[mid]>=target)
+            {
+                second = mid;
+                binaryc(nums,target,start ,mid-1);
+            }   
+            else if(nums[mid]<target)
+            {
+                binaryc(nums,target,mid+1 ,end);
+            }
+        }
     }
     public int[] searchRange(int[] nums, int target) {
-        int pos = binary(nums,target,0,nums.length-1);
-        if(pos == -1)
+        binarys(nums,target,0,nums.length-1);
+
+        if(!found)
             return new int[]{-1,-1};
-            
-        int two[] = new int[2];
-        int i = pos-1;
-        int j = pos+1;
-        
-        while( i>=0 && nums[i] == nums[pos])
-        {
-            i--;
-        }
-        while(j<nums.length && nums[j] == nums[pos])
-        {
-            j++;
-        }
-        two[0] = ++i;
-        two[1] = --j;
-        return two;
+        binary(nums,target,0,nums.length-1);
+        binaryc(nums,target,0,nums.length-1);
+        return new int[]{second,first};
     }
 }
