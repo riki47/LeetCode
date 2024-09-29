a0d6c87c0d444365e9df32e644305e22d1cb504e
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-29 15:26:46 +0530

    Time: 2 ms (72.53%), Space: 41.6 MB (30.44%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int count = 0;
+        int max = 0;
+        int emptys = 0;
+        for(int i= citations.length-1; i>=0; i--)
+        {
+            count++;
+            if(citations[i]<=count)
+            {
+                max = Math.max(max,citations[i]);
+            }
+            else if(citations[i]>0)
+                emptys++;
+        }
+        max = Math.max(emptys,max);
+        return max;
+    }
+}
