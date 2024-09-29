615f8bb55ddd816daf5f2f189bd6cd86de44a8ae
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-29 17:32:06 +0530

    Time: 16 ms (65.36%), Space: 46.3 MB (24.84%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,23 +4,15 @@ class Solution {
         s1.add(nums[nums.length-1]);
         for(int i = nums.length-2; i >= 0; i--)
         {
-            while(s1.peek() <= nums[i])
-            {
+            while(!s1.isEmpty() && s1.peek() <= nums[i])
                 s1.pop();
-                if(s1.isEmpty())
-                    break;
-            }
             s1.push(nums[i]);
         }
         for(int i = nums.length-1; i >= 0; i--)
         {
             int temp = -1;
-            while(s1.peek() <= nums[i])
-            {
+            while(!s1.isEmpty() && s1.peek() <= nums[i])
                 s1.pop();
-                if(s1.isEmpty())
-                    break;
-            }
             if(!s1.isEmpty())
                 temp = s1.peek();
             s1.push(nums[i]);
