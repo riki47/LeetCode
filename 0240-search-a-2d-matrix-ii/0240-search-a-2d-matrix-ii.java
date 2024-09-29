d27f6bc747763af2728b4bcc313e0d1129b9b631
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-09-29 11:29:38 +0530

    Time: 5 ms (99.87%), Space: 46.1 MB (26.58%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row= 0;
+        int col = matrix[0].length-1;
+        while(row < matrix.length && col >=0)
+        {
+            if(matrix[row][col]<target)
+                row++;
+            else if(matrix[row][col]>target)
+                col--;
+            else
+                return true;
+        }
+        return false;
+    }
+}
