329a33ab9f5000bd122ef27262808d0c25e069d3
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2024-10-02 11:10:58 +0530

    Time: 40 ms (25.62%), Space: 62.1 MB (51.49%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        Map<Integer, Integer> valueToRank = new HashMap<>();  
+        int[] sortedUniqueNumbers = Arrays.stream(arr).distinct().sorted().toArray();  //unique sort
+        
+        // Assign ranks to sorted unique elements
+        for (int i = 0; i < sortedUniqueNumbers.length; i++) {
+            valueToRank.put(sortedUniqueNumbers[i], i + 1);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = valueToRank.get(arr[i]);
+        }
+
+        return arr; 
+    }
+}
