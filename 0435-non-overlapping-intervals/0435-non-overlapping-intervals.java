4d1a026f9f98316727c7f681e0a27561a9835efa
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2025-03-26 14:28:46 +0530

    Time: 46 ms (85.97%), Space: 73.4 MB (71.63%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,16 +1,17 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
-        int end = intervals[0][1];
+        //O(nlogn)
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]); //sort according to end time
+        int end = intervals[0][1]; //first meeting ends at this time
         int cnt = 0;
         int n = intervals.length;
+        //O(n)
         for(int i = 1; i<n; i++)
         {
-            if(intervals[i][0] < end)cnt++;
-            else
-            {
-                end = intervals[i][1];
-            }
+          //if second meeting starts before first one ends it cannot be scheduled
+            if(intervals[i][0] < end)cnt++; 
+          //else it can be scheduled update the end time
+            else end = intervals[i][1];
         }
         return cnt;
     }
