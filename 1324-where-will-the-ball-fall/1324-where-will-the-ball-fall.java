6765284c06d6fabb09f80f14d03999e01b6eef0b
Rithwik Koul <66414737+riki47@users.noreply.github.com>
2025-03-31 23:23:21 +0530

    Time: 0 ms (100%), Space: 45.3 MB (91.76%) - LeetHub

--- 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.java
+++ 1324-where-will-the-ball-fall/1324-where-will-the-ball-fall.java
@@ -1,4 +1,27 @@
 class Solution {
+    private void tellPlace(int[][] grid, int row, int col, int ball, int[] ans) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        
+        if (row >= nr) {
+            ans[ball] = col;
+            return;
+        }
+        
+        if (grid[row][col] == 1) {
+            if (col + 1 >= nc || grid[row][col + 1] == -1) {
+                ans[ball] = -1;
+                return;
+            }
+            tellPlace(grid, row + 1, col + 1, ball, ans);
+        } else {
+            if (col - 1 < 0 || grid[row][col - 1] == 1) {
+                ans[ball] = -1;
+                return;
+            }
+            tellPlace(grid, row + 1, col - 1, ball, ans);
+        }
+    }
     public int[] findBall(int[][] grid) {
         int nr = grid.length;
         int nc = grid[0].length;
@@ -8,24 +31,7 @@ class Solution {
             int row = 0;
             int col = ball;
             
-            while (row < nr) {
-                if (grid[row][col] == 1) {
-                    if (col + 1 >= nc || grid[row][col + 1] == -1) {
-                        col = -1;
-                        break;
-                    }
-                    row++;
-                    col++;
-                } else {
-                    if (col - 1 < 0 || grid[row][col - 1] == 1) {
-                        col = -1;
-                        break;
-                    }
-                    row++;
-                    col--;
-                }
-            }
-            ans[ball] = col;
+            tellPlace(grid,row,col,ball,ans);
         }
         return ans;
     }
